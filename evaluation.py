59ccbbf2777fc4968dc30e1266197a9cfd6bb69e
Blake Elias <blakeelias@gmail.com>
2020-08-20 20:42:34 +0000

    Evaluation at multiple scales

--- evaluation.py
+++ evaluation.py
@@ -10,17 +10,40 @@ import projections
 def evolve(M, N_0, num_steps=10):
     return [np.linalg.matrix_power(M, t) @ N_0 for t in range(num_steps)]
 
-def evaluate(M_a=None, M_b=None, g_ba=None, N_a_0=None, num_steps=10):
-    N_b_0 = g_ba @ N_a_0
-    
+def evaluate(M_a=None, projection_method=None, g_bas=None, N_a_0=None, num_steps=10):
+    if not g_bas:
+        g_bas = []
+
+    if not projection_method:
+        projection_method = get_projection_methods()[0]
+
+    M_bs = []
+    N_bs = []
+    N_b_hats = []
+
+    M_bs.append(M_a)
     N_a = evolve(M_a, N_a_0, num_steps=num_steps)
-    N_b = [g_ba @ N_a_t for N_a_t in N_a]
-    N_b_hat = evolve(M_b, N_b_0, num_steps=num_steps)
+    N_bs.append(N_a)
+    N_b_hats.append(N_a)
+
+    for i, g_ba in enumerate(g_bas):
+        g_ba_cum = np.linalg.multi_dot(g_bas[:i+1])
+
+        M_b = projection_method(M_bs[-1], g_ba)
+        N_b_0 = g_ba_cum @ N_a_0
+
+        N_b = [g_ba @ N_b_t for N_b_t in N_bs[-1]]
+        N_b_hat = evolve(M_b, N_b_0, num_steps=num_steps)
+
+        M_bs.append(M_b)
+        N_bs.append(N_b)
+        N_b_hats.append(N_b_hat)
 
     return {
         'N_a': N_a,
-        'N_b': N_b,
-        'N_b_hat': N_b_hat
+        'N_bs': N_bs,
+        'M_bs': M_bs,
+        'N_b_hats': N_b_hats
     }
 
 def get_projection_methods():
