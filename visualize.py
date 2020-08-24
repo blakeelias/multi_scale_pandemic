13ce7a1c9320a37c331ef4ff3c3711d72bc0ed31
Blake Elias <blakeelias@gmail.com>
2020-08-24 02:24:56 +0000

    Plot fine-grained and coarse-grained side by side

--- visualize.py
+++ visualize.py
@@ -17,19 +17,21 @@ def upsample_average(grid, scale_factor=2):
     return upsampled
 
 
-def plot_grid(grid_downsampled, grid_original):
+def plot_grid(grid_downsampled, grid_original, ax=None):
     downsampled_size = (grid_downsampled.shape[0] * grid_downsampled.shape[1])
     original_size = (grid_original.shape[0] * grid_original.shape[1])
     downsample_factor = original_size / downsampled_size
     
     # Matplotlib solution:
-    fig, ax = plt.subplots()
+    if not ax:
+        fig, ax = plt.subplots()
+    
     ax.matshow(np.log(grid_downsampled / downsample_factor + 0.01) / np.log(5.0), cmap=plt.cm.Blues, vmin=0.0, vmax=3.0)
 
     for i in range(grid_downsampled.shape[1]):
         for j in range(grid_downsampled.shape[0]):
             c = grid_downsampled[j, i]
-            ax.text(i, j, f'{c:.1f}', va='center', ha='center')
+            # ax.text(i, j, f'{c:.1f}', va='center', ha='center')
 
     # Seaborn / Pandas / Jupyter solution:
     '''
