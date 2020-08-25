669d847893d85a4aa857cf3d424999017a0e2e9e
Blake Elias <blakeelias@gmail.com>
2020-08-25 20:26:20 +0000

    Remove dummy animation

--- visualize.py
+++ visualize.py
@@ -83,23 +83,3 @@ def animate_results(results):
     # anim_fine_grain, anim_coarse_grain
                                                 
 
-def dummy_animate():
-    fig, ax = plt.subplots(figsize=(5, 3))
-    ax.set(xlim=(-3, 3), ylim=(-1, 1))
-    
-    x = np.linspace(-3, 3, 91)
-    t = np.linspace(1, 25, 30)
-    X2, T2 = np.meshgrid(x, t)
-    
-    sinT2 = np.sin(2*np.pi*T2/T2.max())
-    F = 0.9*sinT2*np.sinc(X2*(1 + sinT2))
-    
-    line = ax.plot(x, F[0, :], color='k', lw=2)[0]
-    
-    def animate(i):
-        line.set_ydata(F[i, :])
-
-    anim = FuncAnimation(
-        fig, animate, interval=100, frames=len(t)-1)
-
-    return anim
